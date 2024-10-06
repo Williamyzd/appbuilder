59ec01844608d6f3fdd75b7f6dc2046219e52365
william <williamyzd@163.com>
2024-10-06 21:45:19 +0800

    初始化

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # appbuilder
 打镜像专用
+1. 根据代码仓库打镜像
+编辑 gits/gits.txt 文件，添加如下信息（默认数组，支持若干个）：
+```
+[{
+    "name": "vllm", # 会根据此名字创建临时文件夹
+    "tag": "vllm-cpu-env:v20241006", # build出的镜像名称
+    "url": "https://github.com/vllm-project/vllm.git", # 代码仓库地址
+    "cmd": "cd vllm && docker build -f Dockerfile.cpu -t vllm-cpu-env:v20241006 --shm-size=4g ." # 构建命令，需要跳转到dockerfile所在目录,可以在此指定build时需要添加的参数，-t 指定的tag 需要和前边的tag参数保持一致，否则会报错
+}]
+基本处理逻辑：
+* 拉取代码到 name 指定的文件夹
+* 执行 cmd 指定的命令
+* 镜像打 tag，镜像名称为 阿里云镜像仓库地址 + tag 指定的值
+* push 镜像到阿里云的镜像仓库
---
+++ scrips/builder.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+import subprocess,os,json
+code_path = 'gits/code.txt'
+base_registry = 'registry.cn-hangzhou.aliyuncs.com/reg_pub/'
+def read_codes(code_path):
+    """
+    读取代码仓库，返回仓库列表
+    
+    Args:
+        code_path (str): 代码文件路径
+    
+    Returns:
+        list: 代码列表
+    
+    """
+    with open(code_path, 'r') as f:
+        codes = json.load(f)
+    return codes
+print(read_codes(code_path))
+
+def clone_push(codes):
+    """
+    克隆并推送镜像到指定的仓库
+    
+    Args:
+        codes (list): 包含需要克隆和推送的镜像信息的列表
+    
+    Returns:
+        None
+    
+    """
+    for code in codes:
+        if len(code) <=0:
+            return
+        app_name = code['name']
+        if os.path.exists(app_name):
+            os.system('cd ~ && rm -rf {}'.format(app_name))
+        num,rs = subprocess.getstatusoutput('git clone {} {} '.format(code,app_name))
+        print('git clone {}'.format(code))
+        if num != 0:
+            print('{} 下载失败,原因：{}'.format(code,rs))
+            return
+        else:
+            print('{} 下载成功'.format(code))
+        build_cmd = code['cmd']
+        new_tag = base_registry + code['tag']
+        num,rs = subprocess.getstatusoutput(build_cmd + " && docker tag {} {}".format(code['tag'],new_tag))
+        if num !=0 :
+            print('{} 构建失败,原因：{}'.format(build_cmd),rs)
+            return
+        else:
+            print('{} 构建成功'.format(build_cmd))
+        num,rs = subprocess.getstatusoutput('docker push {}'.format(new_tag))
+        if num !=0 :
+            print('{} 推送失败,原因：{}'.format(base_registry+app_name,rs))
+            return
+        else:
+            print('{} 推送成功'.format(base_registry+app_name))
+
+clone_push(read_codes(code_path))
+        
+
+
+
+
+
+
+
+
+
+
+    
